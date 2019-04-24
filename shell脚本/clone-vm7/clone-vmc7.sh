3caa7d176ec64d90fbf07604ab80a0b7d697f743
dazengge <dazenggezaici@gmail.com>
2019-04-24 20:09:42 +0800

    addELK

--- shell脚本/clone-vm7/clone-vmc7.sh
+++ shell脚本/clone-vm7/clone-vmc7.sh
@@ -3,13 +3,14 @@
 virpwd='/var/lib/libvirt/images/'
 dmqcow="${virpwd}node.qcow2"
 dmxml="${virpwd}demo.xml"
+name='A'
 echo "请输入一个数值用于标示"
 read -p "number:" num
 
 # create
-cp ${dmxml} /etc/libvirt/qemu/mynode$num.xml
-qemu-img create -b ${dmqcow} -f qcow2 ${virpwd}mynode$num.img 50G
-sed -i "s/<name>centos7.0<\/name>/<name>mynode$num<\/name>/" /etc/libvirt/qemu/mynode$num.xml
-sed -i "s/mynode.qcow2/mynode$num.img/" /etc/libvirt/qemu/mynode$num.xml
+cp ${dmxml} /etc/libvirt/qemu/${name}${num}.xml
+qemu-img create -b ${dmqcow} -f qcow2 ${virpwd}${name}${num}.img 50G
+sed -i "s/<name>centos7.0<\/name>/<name>${name}${num}<\/name>/" /etc/libvirt/qemu/${name}${num}.xml
+sed -i "s/mynode.qcow2/${name}${num}.img/" /etc/libvirt/qemu/${name}${num}.xml
 # start
-virsh define /etc/libvirt/qemu/mynode$num.xml
+virsh define /etc/libvirt/qemu/${name}${num}.xml
